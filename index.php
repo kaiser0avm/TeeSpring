c9cf66442130e057ade29a532ff449f56ada9eba
kaiser0avm <kaiser.avm@gmail.com>
2015-04-29 19:19:37 -0500

    TeeSpring Api example with php
    On this project i show how to make a call to the teespring service to
    retrieve the sales info of a specific user and item they have sold

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ function getNumberSold($twitchUser,$shirtName){
 	$r = array();
 	$url = "http://xnf09ccdo4-3.algolia.io/1/indexes/searchable_campaigns_production/query";//The url to contact the api 
 	$apiId = "XNF09CCDO4";//El id de la api
-	$apiKey = "d0580ff9a6916c276178a3ceb841a4d7";//La llave de la api
+	$apiKey = "5cf4b4f788d542e9e1661cb977480f0dcb5acfdae52786e3bf9593ba8da3ddd4";//La llave de la api
 	$data = '{"params":"query='.$twitchUser.'&hitsPerPage=12&restrictSearchableAttributes=%5B%22name%22%2C%22url%22%2C%22description%22%2C%22tag_names%22%2C%22front_text%22%2C%22back_text%22%2C%22id%22%5D&attributesToRetrieve=%5B%22name%22%2C%22url%22%2C%22tippingpoint%22%2C%22amount_ordered%22%2C%22primary_pic_url%22%2C%22secondary_pic_url%22%2C%22endcost%22%2C%22enddate%22%5D&numericFilters=%5B%22id%3E924650%22%2C%22publicly_searchable%3D1%22%5D&page=0",
 			"apiKey":"'.$apiKey.'",
 			"appID":"'.$apiId.'"}
